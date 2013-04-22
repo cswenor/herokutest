2079178977d3635b6b6165a889e30dfb90ced386
Christopher Swenor <cswenor@gmail.com>
2013-04-22 10:19:53 -0400

    Reset everything back up

--- herokutest/settings/prod.py
+++ herokutest/settings/prod.py
@@ -50,7 +50,7 @@ CACHES = memcacheify()
 
 ########## CELERY CONFIGURATION
 # See: http://docs.celeryproject.org/en/latest/configuration.html#broker-transport
-BROKER_TRANSPORT = 'amqp'
+BROKER_TRANSPORT = 'amqplib'
 
 # Set this number to the amount of allowed concurrent connections on your AMQP
 # provider, divided by the amount of active workers you have.
@@ -68,7 +68,7 @@ BROKER_POOL_LIMIT = 1
 BROKER_CONNECTION_MAX_RETRIES = 0
 
 # See: http://docs.celeryproject.org/en/latest/configuration.html#broker-url
-BROKER_URL = environ.get('RABBITMQ_URL') or environ.get('CLOUDAMQP_URL')
+BROKER_URL = environ.get('RABBITMQ_BIGWIG_URL') or environ.get('CLOUDAMQP_URL')
 
 # See: http://docs.celeryproject.org/en/latest/configuration.html#celery-result-backend
 CELERY_RESULT_BACKEND = 'amqp'
